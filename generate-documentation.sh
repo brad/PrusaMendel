15af852ab2b61fec9c10eb7e0fbb020f05c52638
Brad Pitcher <bradpitcher@gmail.com>
2011-12-06 22:28:39 -0800

    run pdflatex twice to generate the table of contents

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -2,6 +2,7 @@ python thingdoc/thingdoc --graph | dot -Tpng > docs/assembly-graph.png
 python thingdoc/thingdoc;
 cd docs;
 pdflatex documentation.tex;
+pdflatex documentation.tex; # first time gets info needed for contents
 rm documentation.aux;
 rm documentation.log;
 rm documentation.out;
